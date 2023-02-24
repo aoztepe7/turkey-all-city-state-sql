7658d7800249da819472b7155a3a84b12fa86522
Ali Oztepe <68004881+aoztepe7@users.noreply.github.com>
2023-02-24 13:19:26 +0300

    Create Country.sql

---
+++ Country.sql
@@ -0,0 +1,6 @@
+INSERT INTO tbl_country
+(
+  id,created,created_by,last_modified,last_modified_by,status,deleted,name
+)
+VALUES
+("7309ce78-2fd4-4bf5-9eae-a6e8d9c494fc","2023-02-23",null,null,null,1,0,"Turkey");
